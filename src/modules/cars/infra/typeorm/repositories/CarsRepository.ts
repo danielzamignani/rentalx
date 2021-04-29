2da27611ffec9fb4293c7ed30274f4b6fbd93c6e
Daniel <danielzamignani@gmail.com>
2021-04-29 08:30:36 -0300

    The route for listing available cars has been implemented, and can also be filtered by name, category_id or brand

--- src/modules/cars/infra/typeorm/repositories/CarsRepository.ts
+++ src/modules/cars/infra/typeorm/repositories/CarsRepository.ts
@@ -11,6 +11,7 @@ class CarsRepository implements ICarsRepository {
     constructor() {
         this.repository = getRepository(Car);
     }
+
     async create({
         brand,
         category_id,
@@ -41,6 +42,32 @@ class CarsRepository implements ICarsRepository {
         });
 
         return car;
+    }
+
+    async findAvailable(
+        brand?: string,
+        category_id?: string,
+        name?: string
+    ): Promise<Car[]> {
+        const carsQuery = await this.repository
+            .createQueryBuilder("c")
+            .where("available = :available", { available: true });
+
+        if (brand) {
+            carsQuery.andWhere("brand = :brand", { brand });
+        }
+
+        if (category_id) {
+            carsQuery.andWhere("category_id = :category_id", { category_id });
+        }
+
+        if (name) {
+            carsQuery.andWhere("name = :name", { name });
+        }
+
+        const cars = await carsQuery.getMany();
+
+        return cars;
     }
 }
 
