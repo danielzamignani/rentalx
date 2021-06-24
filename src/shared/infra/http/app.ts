9905d21ab7d4b2451d8b191f29d3b415677881e6
Daniel <danielzamignani@gmail.com>
2021-06-24 20:10:58 -0300

    Implemented the ability to save car and avatar images in AWS s3

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -1,6 +1,7 @@
 import "reflect-metadata";
 import express, { NextFunction, Request, Response } from "express";
 import "express-async-errors";
+import "dotenv/config";
 import swaggerUi from "swagger-ui-express";
 
 import "@shared/container";
